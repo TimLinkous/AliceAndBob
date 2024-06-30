166fc3e09f30d46583ec50439d135ed06a880604
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-06-29 21:36:01 -0400

    finished correctly calling method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,24 +8,18 @@ public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        if (name.equals("Alice") || name.equals("Bob")) {
-            System.out.println("Hello " + name + "!");
-        } else{ System.out.println("You are neither Alice nor Bob.");
-        }
+       compute();
         // call the compute() method on that new object
     }
 
-    public void compute() {
-        // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
-
-        // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
+    public static void compute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            System.out.println("Hello " + name + "!");
+        } else{ System.out.println("You are neither Alice nor Bob.");
+        }
 
     }
 }
